6f63b67dcdfb0ca32c247726db5ce78328442852
Kovel Alexadner <xanderkovel@gmail.com>
2022-09-11 21:56:13 +0300

    Added all tables

--- lab-01/generate_db.py
+++ lab-01/generate_db.py
@@ -2,11 +2,16 @@ from email import header
 from russian_names import RussianNames
 import csv
 import random
+import lorem
+import utg
+import wonderwords
 
 params = {
     "number_patient": 5000,
     "number_rooms": 1500,
     "number_doctors": 2000,
+    "number_of_diseases": 1200,
+    "number_of_medicine": 4000,
 } 
 
 def generate_patient():
@@ -66,25 +71,47 @@ def generate_rooms():
 
 
 def generate_diseases():
-    header = ['number', 'floor', 'number of beds', 'room type', 'number of patients']
+    header = ['id', 'name', 'symptoms', 'reasons', 'diagnosis', 'classification']
     
-    with open('./tables/room.csv', 'w', encoding='UTF8') as f:
+    with open('./tables/mental.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         
         writer.writerow(header)
         
-        for i in range(params['number_rooms']):
-            floor = random.randint(1, 10)
-            number_of_beds = random.randint(1, 6)
-            room_type = random.randint(0, 10)
-            number_of_patients = random.randint(0, number_of_beds)
+        for i in range(params['number_of_diseases']):
             
-            data = [i, floor, number_of_beds, room_type, number_of_patients]
+            name = wonderwords.RandomWord().word() + ' ' + wonderwords.RandomWord().word()
+            syptoms = wonderwords.RandomSentence().sentence()
+            reasons = wonderwords.RandomSentence().sentence()
+            diagnosis = wonderwords.RandomSentence().sentence()
+            classification = random.randint(0, 100)
+            
+            data = [i, name, syptoms, reasons, diagnosis, classification]
             writer.writerow(data)
 
 
+def generate_medicines():
+    header = ['id', 'name', 'expiration date', 'recipe', 'contraindications', 'side effects']
+    
+    with open('./tables/medicines.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        
+        writer.writerow(header)
+        
+        for i in range(params['number_of_medicine']):
+            
+            name = wonderwords.RandomWord().word() + ' ' + wonderwords.RandomWord().word() + ' ' + wonderwords.RandomWord().word() + ' ' + wonderwords.RandomWord().word()
+            
+            expiration_data = str(random.randint(1, 30)) + '.' + str(random.randint(1, 12)) + '.' + str(random.randint(2022, 2030))
+            recipe = wonderwords.RandomSentence().sentence()
+            contraindications = wonderwords.RandomSentence().sentence()
+            side_effects = wonderwords.RandomSentence().sentence()
+            
+            data = [i, name, expiration_data, recipe, contraindications, side_effects]
+            writer.writerow(data)
+
 def main():
-    generate_rooms()
+    generate_medicines()
 
 
 if __name__ == "__main__":
